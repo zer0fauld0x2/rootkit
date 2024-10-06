35a52babf54522be53ab188fae5b221d689dc861
zer0fauld0x2 <160667935+zer0fauld0x2@users.noreply.github.com>
2024-10-06 12:12:24 +0300

    Rootkit
    python Rootkit

---
+++ rootkit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os 
+os.system("apt-get install")
+os.system("clear")
+os.system("figlet ROOTKİT ")
+os.system("echo Zer0FaulT Defacer")
+print(""" 
+RootKİT Scanner
+""")
+os.system("sudo apt-get install chkrootkit")
+os.system("clear")
+os.system("chkrootkit")
